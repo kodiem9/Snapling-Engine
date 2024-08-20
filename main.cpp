cc5a57145a573399077e10f98036bcb276cb97a9
Franczesko111 <franczeskostr@gmail.com>
2024-08-20 11:40:51 +0200

    build 406
    release yay!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "source/include/Engine.hpp"
 
 #define BACKGROUND_COLOR Color{ 229, 240, 255, 255 }
-constexpr uint16_t BUILD_NUMBER = 392;
+constexpr uint16_t BUILD_NUMBER = 406;
 
 int main()
 {
