f4856b8d230ef531fcf2f7588e3bae8af3de6411
Franczesko111 <franczeskostr@gmail.com>
2024-08-22 12:59:31 +0200

    Entity y position fix

--- source/Entity.cpp
+++ source/Entity.cpp
@@ -2,7 +2,7 @@
 
 void Entity::Draw(uint16_t window_x, u_int16_t window_y, uint8_t id)
 {
-    DrawRectangle(Global::entities[id].x + window_x + Global::game_window_width / 2 - 25, Global::entities[id].y + window_y + Global::game_window_height / 2 - 25, 50, 50, RED);
+    DrawRectangle(Global::entities[id].x + window_x + Global::game_window_width / 2 - 25, Global::entities[id].y * -1 + window_y + Global::game_window_height / 2 - 25, 50, 50, RED);
 }
 
 void Entity::Update(uint8_t id)
