78b557ab10cb1a0e200b4d7fd27036e70c056fdd
Franczesko111 <franczeskostr@gmail.com>
2024-08-23 14:22:34 +0200

    Fixed entity scale + position

--- source/Entity.cpp
+++ source/Entity.cpp
@@ -2,8 +2,9 @@
 
 void Entity::Draw(uint16_t window_x, u_int16_t window_y, uint8_t id)
 {
-    uint16_t entity_size = 64 * ((float)Global::game_scale / 10);
-    DrawRectangle(Global::entities[id].x + window_x + Global::game_window_width / 2 - entity_size / 2, Global::entities[id].y * -1 + window_y + Global::game_window_height / 2 - entity_size / 2, entity_size, entity_size, RED);}
+    float game_fixed_scale = ((float)Global::game_scale / 10);
+    uint16_t entity_size = 64 * game_fixed_scale;
+    DrawRectangle((Global::entities[id].x * game_fixed_scale) + window_x + Global::game_window_width / 2 - entity_size / 2, (Global::entities[id].y * -game_fixed_scale) + window_y + Global::game_window_height / 2 - entity_size / 2, entity_size, entity_size, RED);}
 
 void Entity::Update(uint8_t id)
 {
