66a203ddca64cfe060fa9c6086e15a785d27d2ad
Franczesko111 <franczeskostr@gmail.com>
2024-08-23 11:00:18 +0200

    build 1139

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "source/include/Engine.hpp"
 
 #define BACKGROUND_COLOR Color{ 229, 240, 255, 255 }
-constexpr uint16_t BUILD_NUMBER = 1007;
+constexpr uint16_t BUILD_NUMBER = 1139;
 
 int main()
 {
